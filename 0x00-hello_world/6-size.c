0e6295ee2ab9c2703b3c7fa49823e0816fbcba9b
Isaac Adebayo <adebayoisonline@gmail.com>
2022-09-08 09:02:18 -0700

    main.c modified

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,7 +14,7 @@ int main(void)
 	float myFloat = 0;
 
 	printf("Size of a char: %d byte(s)\n", sizeof(myChar));
-	printf("Size of a int: %d byte(s)\n", sizeof(myInt));
+	printf("Size of an int: %d byte(s)\n", sizeof(myInt));
 	printf("Size of a long int: %d byte(s)\n", sizeof(myLInt));
 	printf("Size of a long long int: %d byte(s)\n", sizeof(myLlInt));
 	printf("Size of a float: %d byte(s)\n", sizeof(myFloat));
