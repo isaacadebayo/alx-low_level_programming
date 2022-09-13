cd7357597574a4b050dd3f9417cac45c6fc8016f
Isaac Adebayo <adebayoisonline@gmail.com>
2022-09-13 15:47:05 -0700

    Task 3 - Write a function that checks for lowercase character.
    Prototype: int _islower(int c);
    Returns 1 if c is lowercase
    Returns 0 otherwise

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *_islower - a function that checks for lowercase character
+ *@c : input character to the function
+ *Return: the program returns 0
+ */
+
+int _islowe(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}
