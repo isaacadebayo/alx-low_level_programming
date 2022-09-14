550e75bb7cbea17d01c019666d87860d6e34036c
Isaac Adebayo <adebayoisonline@gmail.com>
2022-09-14 07:55:16 -0700

    Task 8 - Write a function that prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59.
    Prototype: void jack_bauer(void);

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -2,7 +2,7 @@
 
 /**
  *jack_bauer - a function that prints every minute
- *of the day of Jack Bauer, starting from 00:00 to 23:59.
+ *of the day of Jack Bauer, starting from 00:00 to 23:59
  *Return: this function does not return any value
  */
 
