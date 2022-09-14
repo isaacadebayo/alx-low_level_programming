55bc7a18938c3f6dd9505dee9ba6cc11de9d3c1a
Isaac Adebayo <adebayoisonline@gmail.com>
2022-09-14 00:44:18 -0700

    Task 7 - Write a function that prints the last digit of a number.
    Prototype: int print_last_digit(int);
    Returns the value of the last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ *print_last_digit - a function that prints the last digit of a number.
+ *@n: input character to the function
+ *Return: the value of the last digit
+ */
+
+int print_last_digit(int n)
+{
+	return (n % 10);
+}
